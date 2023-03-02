b9d61a4b69eedcb13d7f12f62b1a7cad34b435a7
Ziga <ziga.zajc007@gmail.com>
2023-03-02 21:22:20 +0100

    update

--- bin/index.js
+++ bin/index.js
@@ -6,20 +6,20 @@ const { bold, green, red, blue } = require('kleur');
 const { log } = require('./utils.js');
 
 console.log(blue(`
- _____       _     _     _ _     ____        _ _     _           
-|  __ \\     | |   | |   (_) |   |  _ \\      (_) |   | |          
-| |__) |__ _| |__ | |__  _| |_  | |_) |_   _ _| | __| | ___ _ __ 
+ _____       _     _     _ _     ____        _ _     _
+|  __ \\     | |   | |   (_) |   |  _ \\      (_) |   | |
+| |__) |__ _| |__ | |__  _| |_  | |_) |_   _ _| | __| | ___ _ __
 |  _  // _\` | '_ \\| '_ \\| | __| |  _ <| | | | | |/ _\` |/ _ \\ '__|
-| | \\ \\ (_| | |_) | |_) | | |_  | |_) | |_| | | | (_| |  __/ |   
-|_|  \\_\\__,_|_.__/|_.__/|_|\\__| |____/ \\__,_|_|_|\\__,_|\\___|_|   
-                                                                                                                                  
+| | \\ \\ (_| | |_) | |_) | | |_  | |_) | |_| | | | (_| |  __/ |
+|_|  \\_\\__,_|_.__/|_.__/|_|\\__| |____/ \\__,_|_|_|\\__,_|\\___|_|
+
 `));
 
 if(process.argv.length < 3){
-  log("Please provide the path to the project directory.", 'ERROR');
-  log("Example 1 (Current directory): rabbit-builder .");
-  log("Example 2 (Relative path): rabbit-builder Passky/Passky-Client");
-  log("Example 3 (Absolute path): rabbit-builder /home/ziga/Documents/Projects/Passky/Passky-Client");
+	log("Please provide the path to the project directory.", 'ERROR');
+	log("Example 1 (Current directory): rabbit-builder .");
+	log("Example 2 (Relative path): rabbit-builder Passky/Passky-Client");
+	log("Example 3 (Absolute path): rabbit-builder /home/ziga/Documents/Projects/Passky/Passky-Client");
 	return;
 }
 
